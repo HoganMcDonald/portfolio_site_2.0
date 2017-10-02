cc1951b221465117931459ec7d9d7a877a582116
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-10-02 13:22:27 -0500

    migrated experience to ng-repeat. incomplete styling.

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -31,4 +31,24 @@ app.controller('mainController', function($scope, $http) {
         .catch(err => console.log(err));
     } // end validate form
   }; // end sendEmail
+
+  // places I have worked. array of objects.
+  $scope.experience = [
+    {name: 'DeepLearning.ai',
+    description: 'I completed a certification course on building deep learning networks. This involved building an image classifier, implimenting logistic regression, and leveraging Python libraries to handle large, unstructured data sets. I have been using this knowlege to build a web API that can be used to identify local mushrooms.',
+    techUsed: ['Python', 'Numpy', 'Pycharm', 'Flask', 'TensorFlow']},
+    {name: 'Prime Digital Academy',
+    desccription: 'I chose to attend prime because I knew that I learned best in an immersive environment. Prime o ered the intense experience I knew represented the best entry point into the industry for me.',
+    techUsed: ['MEAN Stack', 'SQL', 'SASS', 'JQuery', 'Heroku']},
+    {name: '[words] Bookstore',
+    desccription: 'I worked in collaboration with business owners to open a first-of-its- kind independent bookstore in the MSP airport.',
+    techUsed: ['Adobe Illustrator', 'Adobe Photoshop', 'Netsuite']},
+    {name: 'Bloom Church',
+    description: 'While at Bloom, I was ordained as a minister and would regularly deliver sermons to a group of 30 - 50 people. Other responsibilities included maintaining online content including church website, weekly newsletter, podcasts, and google ads.',
+    techUsed: ['Adobe Illustrator', 'Adobe Photoshop', 'Squarespace', 'Google Ads', 'Google Analytics', 'MailChimp']},
+    {name: 'North Central University',
+    description: 'My background is in pastoral ministry which involves formal education in organizational leadership, research, and communication. This background has taught me to be empithetic towards users in my design process as well as build software that provides value to people\'s lives',
+    techUsed: ['Microsoft Office', 'Apple iWork', 'Google Drive']}
+  ]
+
 }); // end main controller
