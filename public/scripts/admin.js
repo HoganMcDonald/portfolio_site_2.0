fb20e96c9e52da8d44661d4cf14a46ea00dcf739
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-11-29 18:00:04 -0600

    ready to deploy for testing

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -3,8 +3,11 @@ let app = angular.module('admin', ['ngSanitize']);
 app.controller('adminController', function($sanitize, $scope, $http) {
 
   $scope.renderHTML = function() {
-    console.log('triggered');
     $scope.posthtml = $sanitize(marked($scope.postmd));
+  };
+
+  $scope.sendMD = function() {
+    console.log($scope.postmd);
   };
 
 });
